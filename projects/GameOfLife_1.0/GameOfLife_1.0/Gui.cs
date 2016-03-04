3aba8067a57675a03e9e6ae8fc4a9e2788216442
NordstromDevs <kent.nordstrom@gmail.com>
2016-03-04 08:31:17 +0100

    Added GameOfLife_v1.0

---
+++ projects/GameOfLife_1.0/GameOfLife_1.0/Gui.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GameOfLife_1._0
+{
+    class Gui
+    {
+        public void Render(bool[,] cells)
+        {
+            var rows = cells.GetLength(0);
+            var columns = cells.GetLength(1);
+
+            for (int y = 0; y < columns; y++)
+            {
+                for (int x = 0; x < rows; x++)
+                {
+                    var fgColor = cells[x, y] ? ConsoleColor.Cyan : ConsoleColor.Blue;
+                    var bgColor = cells[x, y] ? ConsoleColor.Green : ((x+y)%2==0 ? ConsoleColor.Black : ConsoleColor.DarkGray);
+
+                    RenderCell(x, y, bgColor, fgColor);
+                }
+            }
+        }
+
+        private void RenderCell(int x, int y, ConsoleColor bgColor, ConsoleColor fgColor)
+        {
+            Console.ForegroundColor = fgColor;
+            Console.BackgroundColor = bgColor;
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+        }
+    }
+}
---
+++ projects/GameOfLife_1.0/GameOfLife_1.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameOfLife_1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game(40, 16);
+            var gui = new Gui();
+
+            while (true)
+            {
+                game.GotoNextGeneration();
+                gui.Render(game.Cells);
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
