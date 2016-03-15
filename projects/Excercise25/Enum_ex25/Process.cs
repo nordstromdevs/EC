c325ace10986d5acd55b7ce195b9cfdc81ee075d
NordstromDevs <kent.nordstrom@gmail.com>
2016-03-15 15:16:41 +0100

    Added Excercise 25

---
+++ projects/Excercise25/Enum_ex25/Process.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_ex25
+{
+    class Process
+    {
+        public enum State
+        {
+            Stopped,
+            Running,
+            Pause
+        }
+
+        private State _currentState;
+
+        public State CurrentState
+        {
+            get
+            {
+                return _currentState;
+            }
+        }
+
+        public void Start()
+        {
+            if (_currentState != State.Running)
+                _currentState = State.Running;
+        }
+
+        public void Stop()
+        {
+            if (_currentState == State.Running)
+                _currentState = State.Stopped;
+        }
+
+        public void Pause()
+        {
+            if (_currentState == State.Running)
+                _currentState = State.Pause;
+        }
+    }
+}
---
+++ projects/Excercise25/Enum_ex25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_ex25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myProcess = new Process();
+            Console.WriteLine("Current state: " + myProcess.CurrentState);
+            myProcess.Start();
+            Console.WriteLine("Current state: " + myProcess.CurrentState);
+            myProcess.Pause();
+            Console.WriteLine("Current state: " + myProcess.CurrentState);
+            myProcess.Stop();
+            Console.WriteLine("Current state: " + myProcess.CurrentState);
+            myProcess.Start();
+            Console.WriteLine("Current state: " + myProcess.CurrentState);
+            myProcess.Stop();
+            Console.WriteLine("Current state: " + myProcess.CurrentState);
+
+            Console.ReadKey();
+        }
+    }
+}
